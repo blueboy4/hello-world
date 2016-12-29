c6ac039cea7201a8af71020edcdb5befac454763
Shantanu Joshi <sdjoshi@gmail.com>
2016-12-29 18:09:06 +0530

    First VC++ project (win32 console application)

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,20 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+using namespace std;
+
+#include "stdafx.h"
+#include "iostream"
+
+
+
+int main()
+{
+	int a;
+	cout << "hello world! \n";
+	cin >> a;
+	cout << "a = " << a << "\n";
+	cin >> a;
+    return 0;
+}
+
